c3d2cc608d8ba09096963682efcd5559ef01631d
iltenahmet <iltenahmet@gmail.com>
2026-02-18 18:30:08 -0500

    fix express syntax

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,7 +26,7 @@ app.get('/api/health', (_req, res) => {
 if (process.env.NODE_ENV === 'production') {
   const clientPath = path.join(__dirname, '../../client/dist');
   app.use(express.static(clientPath));
-  app.get('*', (_req, res) => {
+  app.get('/{*splat}', (_req, res) => {
     res.sendFile(path.join(clientPath, 'index.html'));
   });
 }
